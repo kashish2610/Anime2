a0b6aaa59f24c7e9591d1ef34ac7edc0451b21b7
Kashish Sharma <76687274+kashish2610@users.noreply.github.com>
2025-06-23 01:31:45 +0530

    Update Anime2.py

--- Anime2.py
+++ Anime2.py
@@ -7,10 +7,13 @@ import gdown
 url = "https://drive.google.com/uc?id=1VNnKhgB0xNdVq4qL6HbaWvAXIT0h1Gsb"
 similarity= "similarity1.pkl"
 gdown.download(url, similarity, quiet=False)
+with open(similarity, "rb") as f:
+    similarity_matrix = pickle.load(f)
+
 movies_title= movies_df['name'].values
 def recommend(movie):
     movie_index = movies_df[movies_df['name'] == movie].index[0]
-    distance = similarity[movie_index]
+    distance = similarity_matrix[movie_index]
     movie_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
     recommended_movies = []
     for i in movie_list:
@@ -23,7 +26,7 @@ Selected_movie = st.selectbox(
 "Select a Anime",
 movies_title
 )
-if st.button("Recommend", type="tertiary"):
+if st.button("Recommend"):
     recommendations = recommend(Selected_movie)
     for i in recommendations:
         st.write(i)
